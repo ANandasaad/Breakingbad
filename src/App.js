5747113621a3517195491b09c956b5ccb670b283
Anand kushwaha <anandkushwaha70@gmail.com>
2022-10-21 22:25:26 +0530

    breaking bad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import Header from './components/Header';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const App=()=> {
+
+  const [itmes, setItems]=useState([])
+  const [isLoading, setIsLoading] =useState(true)
+
+  useEffect(()=>{
+    const fetchItems =async()=>{
+      const result=await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data);
+      
+    }
+
+    fetchItems();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header/>
     </div>
   );
 }
